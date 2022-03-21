bbcecf2b173427e9b86f098f44eb69e6f4bcc8c8
roman_p <roman_p@gmail.com>
2022-03-21 22:09:43 +0200

    Getting user's name

--- src/com/softserve/edu/App.java
+++ src/com/softserve/edu/App.java
@@ -1,8 +1,13 @@
 package com.softserve.edu;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Enter your name");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
