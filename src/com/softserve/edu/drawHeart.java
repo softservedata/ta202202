c5f374b29e352b1ac6735840cc9ac8e17ba8beeb
DmitryGvozd <d.gvozdetskiy@gmail.com>
2022-03-27 17:23:10 +0300

    Adding the heart drawing logic with (purely symbolic) user input

---
+++ src/com/softserve/edu/drawHeart.java
@@ -0,0 +1,19 @@
+package com.softserve.edu;
+
+public class drawHeart {
+
+    public static void main() { //proudly stolen from https://stackoverflow.com/questions/63934144/draw-a-heart-in-ascii-art-style-based-on-user-input
+        int n = 4;
+        for (int y = -n; y <= 2 * n; y++) {
+            for (int x = -2 * n; x <= 2 * n; x++)
+                if ((y <= 0 &&
+                        ((int) Math.sqrt((x+n)*(x+n) + y*y) <= n
+                                || (int) Math.sqrt((x-n)*(x-n) + y*y) <= n))
+                        || (y > 0 && Math.abs(x) <= 2 * n - y))
+                    System.out.print("♥ ");
+                else
+                    System.out.print("♡ ");
+            System.out.println();
+        }
+    }
+}
