01d31de669d6e7faa217d66b8fbb1f2a1e62fa3c
DmitryGvozd <d.gvozdetskiy@gmail.com>
2022-04-05 18:29:38 +0300

    sanitized inputs in nonparametrized constructor

--- src/com/softserve/edu/hw4/Product.java
+++ src/com/softserve/edu/hw4/Product.java
@@ -2,10 +2,6 @@ package com.softserve.edu.hw4;
 
 import java.util.Scanner;
 
-/**
- * TODO: add null checks to the constructors.
- */
-
 public class Product {
     private String name;
     private Double price;
@@ -19,8 +15,19 @@ public class Product {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Please enter the product name");
         this.name = userInput.nextLine();
+        if (this.name.length() == 0) {
+            this.name = "null";
+            System.err.println("That was very rude. Your product is now named NULL.");
+        }
         System.out.println("Now the product price");
-        this.price = Double.parseDouble(userInput.nextLine());
+        try {
+            this.price = Double.parseDouble(userInput.nextLine());
+        }
+        catch(Exception e) {
+            this.name = "DIRTY CODE BREAKER";
+            this.price = 0.0;
+            System.err.println("Are you TRYING to break the code? Your product is DISQUALIFIED!");
+        }
     }
 
     public String getName() {
