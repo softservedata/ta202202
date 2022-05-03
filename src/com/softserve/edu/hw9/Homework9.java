4ecbe43e15b39f276932e6b25e46934e97975d99
dmytro <ddavydd96@gmail.com>
2022-05-03 09:27:37 +0300

    updated hw9 to avoid exception

--- src/com/softserve/edu/hw9/Homework9.java
+++ src/com/softserve/edu/hw9/Homework9.java
@@ -12,19 +12,18 @@ public class Homework9 {
         arraylist1.add(1);
         arraylist1.add(2);
         arraylist1.add(3);
-        arraylist1.add(4);
+        arraylist1.add(9);
         arraylist2.add(3);
         arraylist2.add(1);
         arraylist2.add(6);
         arraylist2.add(7);
+        arraylist2.add(9);
 
-        //test
-        int iterationLength = Math.max(arraylist1.size(), arraylist2.size());
-        for (int i = 0; i < iterationLength; i++) {
-            if (arraylist2.contains(arraylist1.get(i))) {
-                commonList.add(arraylist1.get(i));
-                System.out.println("Common element in arraylist1 and arraylist2: " + arraylist1.get(i));
-            }
+        for (int i = 0; i < arraylist1.size(); i++) {
+                if (arraylist2.contains(arraylist1.get(i))) {
+                    commonList.add(arraylist1.get(i));
+                    System.out.println("Common element in arraylist1 and arraylist2: " + arraylist1.get(i));
+                }
         }
         System.out.println("List with common items: " + commonList);
     }
