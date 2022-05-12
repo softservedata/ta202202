b4d4ec6134be193455d31f7710a23648bd8614c3
DmitryGvozd <d.gvozdetskiy@gmail.com>
2022-05-12 17:15:06 +0300

    homework 11 adjusted to remove duplicate functions

--- src/com/softserve/edu/hw11/homework11.java
+++ src/com/softserve/edu/hw11/homework11.java
@@ -1,5 +1,7 @@
 package com.softserve.edu.hw11;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,60 +14,46 @@ public class homework11 {
         String password = userInput.nextLine();
         userInput.close();
 
-        // checks individually for digits, lowercase, uppercase and specials then checks for length
-        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@!#$%^&+=.\\-_*])[a-zA-z\\d@!#$%^&+=*.\\-_]{12,}$";
-        Pattern pattern = Pattern.compile(regex);
+        Map<String, String> regexMap = createMap();
+        Pattern pattern = Pattern.compile(regexMap.get("generalRegex"));
         Matcher matcher = pattern.matcher(password);
 
         if(matcher.matches()) {
             System.out.println("That's a strong password!");
         } else {
-            if(!findNumber(password)) {
+            if(didntMatch(password, regexMap.get("numberRegex"))) {
                 System.err.println("There has to be at least one number.");
             }
-            if(!findUpperCase(password)) {
-                System.err.println("There has to be at least upper case letter.");
+            if(didntMatch(password, regexMap.get("upperRegex"))) {
+                System.err.println("There has to be at least one upper case letter.");
             }
-            if(!findLowerCase(password)) {
-                System.err.println("There has to be at least lower case letter.");
+            if(didntMatch(password, regexMap.get("lowerRegex"))) {
+                System.err.println("There has to be at least one lower case letter.");
             }
-            if(!findSpecial(password)) {
+            if(didntMatch(password, regexMap.get("specialRegex"))) {
                 System.err.println("There has to be at least one special character.");
             }
-            if(!findLength(password)) {
+            if(didntMatch(password, regexMap.get("lengthRegex"))) {
                 System.err.println("Password has to be at least 12 symbols long.");
             }
         }
     }
 
-    private static boolean findMatch(String password, String numberRegex) {
-        Pattern numberPattern = Pattern.compile(numberRegex);
-        Matcher matcher = numberPattern.matcher(password);
-        return matcher.find();
+    private static Map<String, String> createMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("generalRegex",
+                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@!#$%^&+=.\\-_*])[a-zA-z\\d@!#$%^&+=*.\\-_]{12,}$");
+        map.put("numberRegex", "[0-9]");
+        map.put("upperRegex", "[A-Z]");
+        map.put("lowerRegex", "[a-z]");
+        map.put("specialRegex", "[@!#$%^&+=.\\-_*]");
+        map.put("lengthRegex", "^.{12,}$");
+        return map;
     }
 
-    private static boolean findNumber(String password) {
-        String numberRegex = "[0-9]";
-        return findMatch(password, numberRegex);
-    }
-
-    private static boolean findUpperCase(String password) {
-        String upperRegex = "[A-Z]";
-        return findMatch(password, upperRegex);
-    }
-
-    private static boolean findLowerCase(String password) {
-        String lowerRegex = "[a-z]";
-        return findMatch(password, lowerRegex);
-    }
-
-    private static boolean findSpecial(String password) {
-        String specialRegex = "[@!#$%^&+=.\\-_*]";
-        return findMatch(password, specialRegex);
-    }
-
-    private static boolean findLength(String password) {
-        String lengthRegex = "^.{12,}$";
-        return findMatch(password, lengthRegex);
+    private static boolean didntMatch(String password, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return !matcher.find();
     }
 }
