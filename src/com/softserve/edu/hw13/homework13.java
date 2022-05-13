2c278290b96bd07d8dc46deb694fc79e2ecec988
DmitryGvozd <d.gvozdetskiy@gmail.com>
2022-05-13 19:37:33 +0300

    homework 13 - indented to increase readability

--- src/com/softserve/edu/hw13/homework13.java
+++ src/com/softserve/edu/hw13/homework13.java
@@ -9,20 +9,25 @@ public class homework13 {
     }
 
     static void modifyFile(String path, String oldString, String newString) {
+
         File file = new File(path);
         StringBuilder oldContent = new StringBuilder();
         BufferedReader reader = null;
         FileWriter writer = null;
+
         try {
             reader = new BufferedReader(new FileReader(file));
             String line = reader.readLine();
+
             while (line != null) {
                 oldContent.append(line).append(System.lineSeparator());
                 line = reader.readLine();
             }
+
             String newContent = oldContent.toString().replaceAll(oldString, newString);
             writer = new FileWriter(file);
             writer.write(newContent);
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
