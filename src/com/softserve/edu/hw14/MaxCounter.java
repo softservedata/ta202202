9f191028623a6d0c7757a8e1548234ea8295d4d6
roman_p <roman_p@gmail.com>
2022-05-22 21:54:36 +0300

    hw14 - Application to count maximal numbers in List using Stream API

---
+++ src/com/softserve/edu/hw14/MaxCounter.java
@@ -0,0 +1,36 @@
+package com.softserve.edu.hw14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MaxCounter {
+    public static void main(String[] args) {
+        longListCreator();
+        printCountOfMax();
+    }
+    private static final List<Long> randomList = new ArrayList<>();
+    private static final long LIST_LENGTH = 10;
+    private static final long UP_TO = 1;
+
+    private static void longListCreator(){
+        long count = LIST_LENGTH;
+        do{
+            randomList.add(Math.round(Math.random()*UP_TO));
+            count--;
+        }
+        while(count>0);
+        printList();
+    }
+
+    private static void printList(){
+        System.out.println("\nThe new created list: ");
+        randomList.forEach(x -> System.out.print(x + " "));
+    }
+
+    private static void printCountOfMax(){
+        System.out.printf("\n\nThe count of maximal numbers is: %d\n",
+                randomList.stream()
+                .filter(aLong -> aLong.equals(randomList.stream().max(Long::compareTo).get())).count());
+    }
+}
